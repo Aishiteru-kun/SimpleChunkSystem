eff8b7ff813eca1ffe67255333b392cbebd4ddaf
Olexsandr Zoshchuk <kutabare.p@gmail.com>
2025-08-18 10:37:19 +0300

    Template

--- Source/SimpleChunkSystem/Public/Manager/ChunkManager_DynamicData.h
+++ Source/SimpleChunkSystem/Public/Manager/ChunkManager_DynamicData.h
@@ -7,6 +7,31 @@
 #include "System/ChunkSystem_DynamicData.h"
 #include "ChunkManager_DynamicData.generated.h"
 
+struct FChunkData_ObjectInfo : public FCellBaseInfo
+{
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Chunk Data")
+	TStrongObjectPtr<UObject> Object;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Chunk Data")
+	FString ObjectName;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Chunk Data")
+	FVector Location;
+
+	virtual bool Serialize(FArchive& Ar) override
+	{
+		Ar << ObjectName;
+		Ar << Location;
+		return true;
+	}
+
+	friend FArchive& operator<<(FArchive & Ar, FChunkData_ObjectInfo & Data)
+	{
+		Data.Serialize(Ar);
+		return Ar;
+	}
+};
+
 UCLASS()
 class SIMPLECHUNKSYSTEM_API UChunkManager_DynamicData : public UChunkManagerBase
 {
